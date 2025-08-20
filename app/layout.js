e1b0c01d9257e38addfd2246060c4217762c3763
Subh <subhranshukhilar@outlook.com>
2025-08-20 16:25:47 +0530

    Added cropped favicon

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,11 @@ import "./globals.css";
 
 
 export const metadata = {
-  title: "Shovana",
+  title: "Shovana Charity Foundation",
   description: "Bring Change Home",
+  icons: {
+    icon: "/favicon-cropped.png", 
+  },
 };
 
 export default function RootLayout({ children }) {
